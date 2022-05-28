22be1e16d0276f7d4300723823eb73bd0384250f
Bruno Dias <dias.h.bruno@gmail.com>
2022-05-28 20:00:19 -0300

    [feature] instantiate any recipient with capacity and quantity.

--- spec/testing.rb
+++ spec/testing.rb
@@ -3,16 +3,64 @@
 require 'pry'
 require 'rantly/rspec_extensions'
 
-class Recipient; end
+class Recipient
+  attr_accessor :capacity, :quantity
 
-class Bucket < Recipient; end
+  def self.filled(capacity, quantity)
+    new(capacity, quantity)
+  end
+
+  def self.empty(capacity)
+    filled(capacity, 0)
+  end
+
+  def initialize(capacity, quantity)
+    @capacity = capacity
+    @quantity = quantity
+  end
+end
+
+class Bucket < Recipient
+  def fill(cup)
+    @quantity += cup.quantity
+    self
+  end
+
+  def full?
+    @quantity == @capacity
+  end
+end
 
 class Cup < Recipient; end
 
+RSpec.describe Cup do
+  describe 'instantiation' do
+    it 'initialize with quantity' do
+      expect do
+        described_class.filled(1, 1)
+      end.not_to raise_exception
+    end
+
+    it 'initialize empty' do
+      expect do
+        described_class.empty(1)
+      end.not_to raise_exception
+    end
+  end
+end
+
 RSpec.describe Bucket do
   describe 'instantiation' do
-    it 'classes' do
-      expect(described_class.new.nil? || Cup.new.nil?).to be(false)
+    it 'initialize with quantity' do
+      expect do
+        described_class.filled(1, 1)
+      end.not_to raise_exception
+    end
+
+    it 'initialize empty' do
+      expect do
+        described_class.empty(1)
+      end.not_to raise_exception
     end
   end
 end
